d57ca1fe8cbed665c3e20a40a30397c40a860014
Marwa Ragheb <marwa.ragheb@gmail.com>
2020-07-02 01:53:27 +0000

    Initial commit with all my work so far

---
+++ if-statement.js
@@ -0,0 +1,8 @@
+const fruit = 'orange';
+const x = fruit.length;
+if (x <= 5) {
+    console.log('The fruit name has more than five characters.');
+
+} else {
+    console.log('The fruit name has more than five characters.');
+}
